c44b9625dca7c947cd102f81ca076d8566a6464a
Gowtham Kumar S <139731660+gowtham06offl@users.noreply.github.com>
2025-04-15 19:17:56 +0530

    Add files via upload

---
+++ Subtraction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter the Value of A:");
+    scanf("%d",&a);
+    printf("Enter the Value of B:");
+    scanf("%d",&b);
+    c = a+b;
+    printf("The Addition of A and B are :%d",c);
+    return 0;
+}
